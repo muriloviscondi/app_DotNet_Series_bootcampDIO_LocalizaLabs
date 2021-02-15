5a71c4974fc5716a6efc2a7ea328df6e751815d4
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2021-02-15 13:29:54 -0300

    criação do console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_DotNet_Series_bootcampDIO_LocalizaLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
